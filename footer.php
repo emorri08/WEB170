a588a08b73a6f1d48d27605b2bfc62492fb8ec9e
emorri08 <emorri08@seattlecentral.edu>
2017-05-03 21:31:08 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,37 @@
+<!-- cut here for php includes footer -->
+        <footer>
+<!-- begin footer navigation -->
+            <nav id="foot-nav">
+                <ul id="secondary-nav">
+                    <li>
+                        <a class="nav" href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="nav" href="#">About</a>
+                            <ul>
+                                <li>
+                                    <a class="nav" href="#">Sub-page Three</a>
+                                </li>
+                                <li>
+                                    <a class="nav" href="#">Sub-page Two</a>
+                                </li><!-- There are two absolutes in this universe....We're all gonna die and FARTS ARE FUNNY. -->
+                                <li>
+                                    <a class="nav" href="#">Sub-page Three</a>
+                                </li>
+                            </ul>
+                    </li>
+                    <li>
+                        <a class="nav" href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a class="nav" href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+<!-- end footer navigation -->
+            <p class="copy">
+                <small>&copy;<?php echo date('Y'); ?> E. Boyd, <a href="http://3tabbies.com" target="_blank">3 Tabbies</a>, All Rights Reserved. </small>
+            </p>
+        </footer>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head><!-- Oh everything's cruel according to you. Keeping him chained us in the backyard is cruel. Pulling his tail is cruel. Yelling in his ears is cruel. Everything is cruel. So excuse me if I'm cruel. -Homer Simpson-->
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="robots" content="noindex,nofollow" />
+                <title>Poop One - Told you I was gonna use this naming convention =^..^= </title>
+            <script src="script.js"></script>
+        <link rel="stylesheet" href="style.css">
+    </head>
+<!-- end of invisible content -->
+    <body>
+        <header>
+            <img class="morrison" src="images/morrison01.svg" alt="the morrisons"/>
+ <!--           <h1>The Morrisons</h1>  -->
+            <h2>Family Photo Gallery &amp; Blog</h2>
+<!--begin navigation-->
+                <nav id="top-nav">
+                    <ul id="main-nav">
+                        <li>
+                            <a class="nav" href="#">Home</a>
+                        </li>
+                        <li>
+                            <a class="nav" href="#">About</a>
+                                <ul>
+                                    <li>
+                                        <a class="nav" href="#">Sub-page One</a>
+                                    </li>
+                                    <li>
+                                        <a class="nav" href="#">Sub-page Two</a>
+                                    </li>
+                                    <li>
+                                        <a class="nav" href="#">Sub-page Three</a>
+                                    </li>
+                                </ul>
+                        </li>
+                        <li>
+                            <a class="nav" href="#">Blog</a>
+                        </li>
+                        <li>
+                            <a class="nav" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+<!--end navigation-->
+        </header>
+<!-- cut here for php includes header -->
