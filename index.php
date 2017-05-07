586e8707cddff5c473dc62912e383ae552574dac
emorri08 <emorri08@seattlecentral.edu>
2017-05-06 17:21:47 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php get_header(); #get header.php ?>
     <div id="content" class="page">
-<?php if(have_posts()) : while(have_posts()) : the_post(); #the ifwhile loop ?>
-        <article id="page-content-<?php the_ID(); ?>" class="page-content">
-            <h2><?php the_title(); #insert the title?></h2>
-<?php the_content(''); #the actual content of the page ?>
-        </article>
-<?php endwhile; endif; #end the ifwhile loop ?>   
-
+<?php if ( have_posts()) : while ( have_posts()) : the_post(); ?>
+        
+        <h2><a href="<?php the_permalink();?><?php the_title(); ?>"></a></h2>
+<?php the_content(''); ?>        
+<?php endwhile; endif; ?>
     </div>
 <?php get_footer(); #get footer.php?>
