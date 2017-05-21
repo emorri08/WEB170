941c435e611063216723423a29e06adc07c3051b
emorri08 <emorri08@seattlecentral.edu>
2017-05-21 02:09:41 -0700

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,34 +1,18 @@
 <!-- cut here for php includes footer -->
         <footer>
 <!-- begin footer navigation -->
-            <div id="secondary">
-                <nav id="foot-nav">
-                    <ul id="secondary-nav">
-                        <li>
-                            <a class="foot-nav" href="home">Home</a>
-                        </li>
-                        <li>
-                            <a class="foot-nav" href="blog">Blog</a>
-                        </li>
-                        <li>
-                            <a class="foot-nav" href="about">About</a>
-                                <ul>
-                                    <li>
-                                        <a class="sub-foot-nav" href="about/sub-page-one">Sub-page One</a>
-                                    </li>
-                                    <li>
-                                        <a class="sub-foot-nav" href="about/sub-page-two">Sub-page Two</a>
-                                    </li><!-- There are two absolutes in this universe....We're all gonna die and FARTS ARE FUNNY. -->
-                                    <li>
-                                        <a class="sub-foot-nav" href="about/sub-page-three">Sub-page Three</a>
-                                    </li>
-                                </ul>
-                        </li>
-                        <li>
-                            <a class="foot-nav" href="contact">Contact</a>
-                        </li>
-                    </ul>
-                </nav>
+            <div id="sub-menu">
+                <ul id="sub-nav">
+                    <li>
+                        <a class="sub-foot-nav" href="about/sub-page-one">Sub-page One</a>
+                    </li>
+                    <li>
+                        <a class="sub-foot-nav" href="about/sub-page-two">Sub-page Two</a>
+                    </li><!-- There are two absolutes in this universe....We're all gonna die and FARTS ARE FUNNY. -->
+                    <li>
+                        <a class="sub-foot-nav" href="about/sub-page-three">Sub-page Three</a>
+                    </li>
+                </ul>
             </div>
 <!-- end footer navigation -->
             <p class="copy">
