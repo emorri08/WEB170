6c6b11889625a1edab72bbda4d6c54dca92cc684
emorri08 <emorri08@seattlecentral.edu>
2017-05-24 07:48:32 -0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<!-- SIDEBAR.PHP -->
+<aside>
+    
+<!--SUB MENU -->
+    <div id="sub-navigation" class="widget">
+        
+        <h2 class="sub-navigation-title"><?php echo $parentname; ?></h2>
+    
+        <ul class="sub-navigation-items">
+            <?php echo $children; ?>    
+        </ul>
+    </div>
+<!-- END SUB MENU -->
+
+</aside>
