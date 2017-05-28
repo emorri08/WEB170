6b95a16b5f002f0b6b443ca2536aa83830dedad3
emorri08 <emorri08@seattlecentral.edu>
2017-05-27 17:42:10 -0700

    Add files via upload

--- front-page.php
+++ front-page.php
@@ -1,18 +1,24 @@
 <?php get_header(); ?>
 <div id="content">
-
-    <h2 id="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php the_content(); ?>
-    <!-- left column -->
-        <section id="middle">   
-                <h3 id="rightMainContent">Right Column/Main Page Content</h3>
-                    <div id="paragraph">
-                        <p class="rightMain">Cats making all the muffins eat owner's food so chase laser howl uncontrollably for no reason. Show belly put butt in owner's face so where is my slave? I'm getting hungry. Always hungry eat a plant, kill a hand plays league of legends soft kitty warm kitty little ball of furr lies down . Find something else more interesting chase imaginary bugs. Hide at bottom of staircase to trip human lick the plastic bag. Scratch at the door then walk away russian blue or find empty spot in cupboard and sleep all day yet find a way to fit in tiny box ears back wide eyed poop in litter box, scratch the walls woops poop hanging from butt must get rid run run around house drag poop on floor maybe it comes off woops left brown marks on floor human slave clean lick butt now. Hide when guests come over rub face on everything, for this human feeds me, i should be a god hide at bottom of staircase to trip human hiss at vacuum cleaner. Purrrrrr intrigued by the shower have secret plans licks your face. Sit on the laptop kitty loves pigs, but chase mice going to catch the red dot today going to catch the red dot today purr for no reason, proudly present butt to human but lay on arms while you're using the keyboard. Lounge in doorway caticus cuteicus give attitude. Meowing non stop for food cats go for world domination but purr for no reason. With tail in the air knock over christmas tree, have my breakfast spaghetti yarn yet sit on human.
-                        </p><!-- I bet Einstein turned himself all sorts of colors before he invented the lightbulb. -Homer Simpson -->
-                    </div>
-        </section>
-<!--end left column -->
-    <small>front-page.php</small>
+    <section class="widget-item">
+        <h2 id="headline">About:</h2>
+            <?php if(have_posts()) : while(have_posts()) : the_post(); //start first loop ?>
+            <?php the_content(); ?>
+            <?php endwhile; endif; //end first loop ?><!-- I bet Einstein turned himself all sorts of colors before he invented the lightbulb. -Homer Simpson -->
+    </section>
+    <section class="widget-item">
+        <h2>Latest Blog Posts:</h2>
+            <ul>
+                <?php rewind_posts();   //stops the first loop ?>
+                <?php query_posts('showposts=4'); //instructions for loop two ?>
+                <?php while(have_posts()) : the_post(); ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+    </section>
+        <small>front-page.php</small>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
